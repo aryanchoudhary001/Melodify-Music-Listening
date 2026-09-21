b97464c77eb9ffa8a41525ddf8d3b22d522b7d0f
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-21 22:01:18 +0530

    save

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-console.log(" here is my java script code ")
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+
+    let as = div.getElementsByTagName("a");
+
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+
+        const element = as[index];
+
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href);
+        }
+    }
+
+    return songs;
+}
+
+
+async function main() {
+
+    // Get all songs
+    let songs = await getSongs();
+
+    console.log(songs);
+
+
+    // Get UL
+    let songUL = document
+        .querySelector(".songList")
+        .getElementsByTagName("ul")[0];
+
+
+    // Add songs to sidebar
+    for (const song of songs) {
+
+        let songName = decodeURIComponent(
+            song.split("/").pop().replace(".mp3", "")
+        );
+
+        songUL.innerHTML += `
+            <li>${songName}</li>
+        `;
+    }
+
+
+    // Play first song
+    var audio = new Audio(songs[0]);
+
+    audio.play();
+
+
+    audio.addEventListener("timeupdate", () => {
+
+        let duration = audio.duration;
+
+        console.log(
+            audio.duration,
+            audio.currentSrc,
+            audio.currentTime
+        );
+
+    });
+}
+
+main();
