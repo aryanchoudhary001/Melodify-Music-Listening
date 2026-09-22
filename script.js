cfd866d1ec73521dd8b35b2183ce2b1ac40d847c
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-22 23:09:49 +0530

    library created

--- script.js
+++ script.js
@@ -1,72 +1,34 @@
 async function getSongs() {
-
-    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let a = await fetch("songs/");
     let response = await a.text();
-
+    
     let div = document.createElement("div");
     div.innerHTML = response;
-
-    let as = div.getElementsByTagName("a");
-
+    let anchors = div.getElementsByTagName("a");
+    
     let songs = [];
-
-    for (let index = 0; index < as.length; index++) {
-
-        const element = as[index];
-
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href);
+    for (let index = 0; index < anchors.length; index++) {
+        const element = anchors[index];
+        // Check if the link points to an audio file (adjust extension if needed, e.g., .m4a or .mp3)
+        if (element.href.endsWith(".mp3") || element.href.endsWith(".m4a")) {
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
-
     return songs;
 }
 
-
 async function main() {
-
-    // Get all songs
+    // Get the list of all songs
     let songs = await getSongs();
-
     console.log(songs);
 
-
-    // Get UL
-    let songUL = document
-        .querySelector(".songList")
-        .getElementsByTagName("ul")[0];
-
-
-    // Add songs to sidebar
+    // Show the first song or all songs in the playlist UI
+    let songUL = document.querySelector(".songList ul");
+    songUL.innerHTML = "";
+    
     for (const song of songs) {
-
-        let songName = decodeURIComponent(
-            song.split("/").pop().replace(".mp3", "")
-        );
-
-        songUL.innerHTML += `
-            <li>${songName}</li>
-        `;
+        songUL.innerHTML += `<li>${decodeURIComponent(song)}</li>`;
     }
-
-
-    // Play first song
-    var audio = new Audio(songs[0]);
-
-    audio.play();
-
-
-    audio.addEventListener("timeupdate", () => {
-
-        let duration = audio.duration;
-
-        console.log(
-            audio.duration,
-            audio.currentSrc,
-            audio.currentTime
-        );
-
-    });
 }
 
 main();
