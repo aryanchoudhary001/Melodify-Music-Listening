59b44636be41633c52fc6f79764a960a3efd42b7
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-23 20:08:22 +0530

    library

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ async function getSongs() {
     let songs = [];
     for (let index = 0; index < anchors.length; index++) {
         const element = anchors[index];
-        // Check if the link points to an audio file (adjust extension if needed, e.g., .m4a or .mp3)
         if (element.href.endsWith(".mp3") || element.href.endsWith(".m4a")) {
             songs.push(element.href.split("/songs/")[1]);
         }
@@ -22,12 +21,23 @@ async function main() {
     let songs = await getSongs();
     console.log(songs);
 
-    // Show the first song or all songs in the playlist UI
+    // Show all songs dynamically in the playlist UI
     let songUL = document.querySelector(".songList ul");
     songUL.innerHTML = "";
     
     for (const song of songs) {
-        songUL.innerHTML += `<li>${decodeURIComponent(song)}</li>`;
+        // Decode URI and clean up the file extension for a nicer display name
+        let cleanName = decodeURIComponent(song).replace(".mp3", "").replace(".m4a", "");
+
+        songUL.innerHTML += `
+            <li>
+                <img class="invert" src="music.svg" alt="">
+                <div class="info">
+                    <div>${cleanName}</div>
+                    <div>Unknown Artist</div>
+                </div>
+                <img class="invert" src="play.svg" alt="">
+            </li>`;
     }
 }
 
