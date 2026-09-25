650b1bc9705c797bff4d9d00da45b71b62274efe
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-25 23:37:38 +0530

    done playbar

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
 let currentSong = new Audio();
+// Global variable to store the loaded playlist so it can be accessed inside click listeners
+let globalSongsList = []; 
+
 async function getSongs() {
     let a = await fetch("songs/");
     let response = await a.text();
-    
-    
     let div = document.createElement("div");
     div.innerHTML = response;
     let anchors = div.getElementsByTagName("a");
-    
     let songs = [];
     for (let index = 0; index < anchors.length; index++) {
         const element = anchors[index];
@@ -18,29 +17,37 @@ async function getSongs() {
     }
     return songs;
 }
-const playMusic =(track)=>{
-    //let audio = new Audio("/songs/"+track)
-    currentSong.src ="/songs/"+track
-    currentSong.play()
-}
+
+const playMusic = (track) => {
+    currentSong.src = "/songs/" + track;
+    currentSong.play();
+    play.src = "pause.svg";
+    
+    /* 
+      🛠️ FIX 1: Cleaned the display name for the playbar.
+      Using decodeURIComponent() removes the '%20' markers.
+      Using .replace() strips off the file extensions like .mp3 and .m4a.
+    */
+    let displayName = decodeURIComponent(track).replace(".mp3", "").replace(".m4a", "");
+    document.querySelector(".songinfo").innerHTML = displayName;
+    
+    document.querySelector(".songtime").innerHTML = "00:00/00:00";
+};
 
 async function main() {
-
-    
-    // Get the list of all songs
-    let songs = await getSongs();
-    console.log(songs);
+    // Get the list of all songs and save them to the global variable
+    globalSongsList = await getSongs();
+    console.log(globalSongsList);
 
     // Show all songs dynamically in the playlist UI
     let songUL = document.querySelector(".songList ul");
     songUL.innerHTML = "";
-    
-    for (const song of songs) {
-        // Decode URI and clean up the file extension for a nicer display name
-        let cleanName = decodeURIComponent(song).replace(".mp3", "").replace(".m4a", "");
-
+    for (const song of globalSongsList) {
+        let cleanName = decodeURIComponent(song)
+            .replace(".mp3", "")
+            .replace(".m4a", "");
         songUL.innerHTML += `
-            <li>
+            <li data-song="${encodeURIComponent(song)}">
                 <img class="invert" src="music.svg" alt="">
                 <div class="info">
                     <div>${cleanName}</div>
@@ -49,14 +56,42 @@ async function main() {
                 <img class="invert" src="play.svg" alt="">
             </li>`;
     }
-    //attach an event listeners to each song
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
-        e.addEventListener("click",element=>{
-            console.log(e.querySelector(".info").firstElementChild.innerHTML)
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim()+".mp3")
-        })
-        
-    })
+
+    // Attach event listeners to each library list item song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            let song = decodeURIComponent(e.dataset.song);
+            console.log(song);
+            playMusic(song);
+        });
+    });
+
+    // Attach an event listener to the playbar's main play button
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            /* 
+              🛠️ FIX 2: Check if a song has already been loaded or is currently selected.
+              The logic checks if the audio element's source is empty or points to the root directory.
+            */
+            if (!currentSong.src || currentSong.src === window.location.href) {
+                /* 
+                  🛠️ FIX 3: Automatically load and play the first song in your array 
+                  if the user clicks the playbar button before selecting a specific item.
+                */
+                if (globalSongsList.length > 0) {
+                    playMusic(globalSongsList[0]);
+                }
+            } else {
+                // If a song was already selected or paused mid-track, simply resume playback
+                currentSong.play();
+                play.src = "pause.svg";
+            }
+        } else {
+            // Pause the music if it's currently running
+            currentSong.pause();
+            play.src = "play.svg";
+        }
+    });
 }
 
 main();
