e4427a1d63df4d0ff91d034dbd2e7a8c0dbd9463
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-26 09:54:47 +0530

    done

--- script.js
+++ script.js
@@ -110,6 +110,13 @@ async function main() {
     currentSong.addEventListener("timeupdate",()=>{
         console.log(currentSong.currentTime,currentSong.duration);
         document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)}/${formatTime(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration)*100 + "%";
+    })
+    //add an event listener to seekbar
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width)*100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration)*percent)/100
     })
 }
 
